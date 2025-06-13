f3cf43a3e6aa418b20e1366678103ea021c327f0
Nicolá Domingues <nsdomingues.developer@gmail.com>
2025-06-13 15:21:02 +0100

    Initial commit

---
+++ FinanceNewsApp/FinanceNewsAppTests/FinanceNewsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceNewsAppTests.swift
+//  FinanceNewsAppTests
+//
+//  Created by Nicolá Domingues on 13/06/2025.
+//
+
+import Testing
+@testable import FinanceNewsApp
+
+struct FinanceNewsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
